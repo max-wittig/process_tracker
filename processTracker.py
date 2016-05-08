ac1e64cda7cbfccc72a9d762c05d6ec6cdeed5db
spaghettic0der <max.wittig95@gmail.com>
2016-05-08 19:32:16 +0200

    you can now choose, which processes to track

--- processTracker.py
+++ processTracker.py
@@ -19,7 +19,7 @@ class JsonWriter:
 
 
 class TimeTracker:
-    def __init__(self):
+    def __init__(self, processes_to_track):
         self.process_list = []
         self.json_writer = JsonWriter()
         self.running = False
@@ -41,13 +41,15 @@ class TimeTracker:
     def start_logging(self, delay):
         print("Logging started...")
         self.running = True
+
         while self.running:
             for proc in psutil.process_iter():
-                process_object = {"name": proc.name(), "count": 1}
-                if self.is_object_already_in_process_list(process_object):
-                    self.process_list[self.get_object_position_with_key(process_object["name"])]["count"] += 1
-                else:
-                    self.process_list.append(process_object)
+                if str(proc.name()).strip(" ") in processes_to_track:
+                    process_object = {"name": proc.name(), "count": 1}
+                    if self.is_object_already_in_process_list(process_object):
+                        self.process_list[self.get_object_position_with_key(process_object["name"])]["count"] += 1
+                    else:
+                        self.process_list.append(process_object)
             time.sleep(delay)
 
     def stop_logging(self):
@@ -58,15 +60,22 @@ class TimeTracker:
         print(self.process_list)
 
     def write_process_list(self):
+        print(json_writer.filename + " written")
         self.json_writer.write_process_list_to_file(self.process_list)
 
 if __name__ == '__main__':
-    time_tracker = TimeTracker()
+    processes_to_track_string = input("Input all processes to track, separated by space\n")
+    processes_to_track = processes_to_track_string.split(' ')
+    print(processes_to_track)
+    time_tracker = TimeTracker(processes_to_track)
     json_writer = JsonWriter()
     thread = threading.Thread(target=time_tracker.start_logging, args=(1, ))
+    thread.setDaemon(True)
     thread.start()
     time.sleep(5)
+    input("Press return to stop logging\n")
     time_tracker.stop_logging()
+    thread.join()
     time_tracker.write_process_list()
 
 
