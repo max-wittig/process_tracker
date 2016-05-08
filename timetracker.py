7a42c2b6fc392f9321f0d0ba8bd4c1088d41cff4
spaghettic0der <max.wittig95@gmail.com>
2016-05-08 14:25:20 +0200

    make basic logging possible

--- timetracker.py
+++ timetracker.py
@@ -9,7 +9,7 @@ class JsonWriter:
         self.filename = "log.json"
 
     def open_create_file(self):
-        self.file = open(self.filename, "a")
+        self.file = open(self.filename, "w")
 
     def write_process_list_to_file(self, process_list):
         self.open_create_file()
@@ -22,10 +22,28 @@ class TimeTracker:
         self.process_list = []
         self.json_writer = JsonWriter()
 
+    def get_object_position_with_key(self, key):
+        counter = 0
+        for process in self.process_list:
+            if process["name"] in str(key):
+                return counter
+            counter += 1
+
+    def is_object_already_in_process_list(self, object_to_check):
+        is_contained = False
+        for thing in self.process_list:
+            if str(thing["name"]) in str(object_to_check["name"]):
+                print(thing["name"] + "==" + object_to_check["name"])
+                is_contained = True
+        return is_contained
+
     def start_logging(self):
         for proc in psutil.process_iter():
-            process_object = {"name": proc.name(), "cpuTime": proc.cpu_percent()}
-            self.process_list.append(process_object)
+            process_object = {"name": proc.name(), "count": 1}
+            if self.is_object_already_in_process_list(process_object):
+                self.process_list[self.get_object_position_with_key(process_object["name"])]["count"] += 1
+            else:
+                self.process_list.append(process_object)
 
     def print_process_list(self):
         print(self.process_list)
@@ -37,9 +55,9 @@ if __name__ == '__main__':
     time_tracker = TimeTracker()
     json_writer = JsonWriter()
 
-    for i in range(0, 10):
-        time_tracker.start_logging()
-        time.sleep(1)
+for i in range(0, 1000):
+    time_tracker.start_logging()
+    time.sleep(1)
     time_tracker.write_process_list()
 
 
