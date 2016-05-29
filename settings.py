8c849eeaff2c676558ee8c398d2979b7374dc93c
spaghettic0der <max.wittig95@gmail.com>
2016-05-29 14:12:39 +0200

    changed settings format

--- settings.py
+++ settings.py
@@ -6,10 +6,21 @@ class Settings:
         self.filename = filename
         self.file = None
         self.processes_to_track = []
+        self.time_delay = 5
+        self.file_content_object = None
+        self.read_json()
 
     def get_processes_to_track(self):
-        return json.loads(self.get_json())
+        return self.processes_to_track
 
-    def get_json(self):
+    def get_time_delay(self):
+        return self.time_delay
+
+    def parse(self):
+        self.processes_to_track = self.file_content_object["processes_to_track"]
+        self.time_delay = self.file_content_object["time_delay"]
+
+    def read_json(self):
         self.file = open("settings/" + self.filename)
-        return self.file.read()
+        self.file_content_object = json.loads(self.file.read())
+        self.parse()
